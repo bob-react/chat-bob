88fcc48f9efd97d17053cfeb490b90972e450b3e
bob <practicas2460@gmail.com>
2020-11-13 13:59:39 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,45 @@
+
+const express    = require('express');
+const http       = require('http');
+const socketio   = require('socket.io');
+const path       = require('path');   
+const Sockets = require('./sockets');
+
+
+class Server{
+
+  constructor(){
+      this.app = express();
+      this.port = process.env.PORT;
+
+      // This server http
+
+      this.server = http.createServer(this.app);
+
+    this.io = socketio(this.server, { /* Configuracion des los sockets */ });
+  }
+
+  middlewares(){
+    this.app.use(express.static(path.resolve(__dirname, '../public')));
+  }
+
+
+  configurarSockets(){
+    new Sockets(this.io);
+  }
+
+  execute(){
+      this.middlewares();
+
+      this.configurarSockets();
+      this.server.listen(this.port, ()=>{
+
+          console.log('Server con clases desde el puerto', this.port)
+      })
+  }
+
+
+}
+
+
+module.exports = Server;
